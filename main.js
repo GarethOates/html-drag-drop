37a35552bfd0d73821cb7bb776fa5a45200a1e46
Gareth Oates <goatie@gmail.com>
2017-03-03 01:26:53 +0100

    Initial Commit

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
 
-    var addValve = document.getElementById("addValve"),
+    var addValve = document.getElementById("addParagraph"),
         canvas   = document.getElementById("Canvas");
 
-    addValve.addEventListener("click", clickHandler);
+    addParagraph.addEventListener("click", clickHandler);
 
     function clickHandler(e) {
-        var name      = document.getElementById("tagName"),
+        var name      = document.getElementById("name"),
             tag       = name.value,
             newButton = document.createElement("p");
 
